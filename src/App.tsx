bad9e62a4e7b8b3db5c804749ea0f9a8e1257050
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2025-05-01 22:15:18 -0400

    added theme provider from shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import About from "./components/About/About";
-import Activities from "./components/Activities/Activities";
-import Connect from "./components/Connect/Connect";
-// import Navbar from "./components/Navbar/Navbar";
-import Profile from "./components/Profile/Profile";
-import Projects from "./components/Projects/Projects";
-import { fadeInTableRows } from "./utils/fadeInTable";
-import { initializeTilt } from "./utils/tilt";
+import About from "@/components/About/About";
+import Activities from "@/components/Activities/Activities";
+import Connect from "@/components/Connect/Connect";
+// import Navbar from "@/components/Navbar/Navbar";
+import Profile from "@/components/Profile/Profile";
+import Projects from "@/components/Projects/Projects";
+import { ThemeProvider } from "@/providers/theme-provider";
+import { fadeInTableRows } from "@/utils/fadeInTable";
+import { initializeTilt } from "@/utils/tilt";
 
 function App() {
   // Initialize card-tilt and table-fade effects on window load
@@ -15,41 +16,43 @@ function App() {
   };
 
   return (
-    <div className="scroll-smooth ">
-      {/* <Navbar /> */}
+    <ThemeProvider>
+      <div className="scroll-smooth ">
+        {/* <Navbar /> */}
 
-      <div className="w-3/4 flex flex-col gap-12 mx-auto mt-56 font-[Courier_New]">
-        {/* TODO: navigate to `components/Profile/Profile.tsx and update all TODO:s */}
-        <Profile />
+        <div className="w-3/4 flex flex-col gap-12 mx-auto mt-56 font-[Courier_New]">
+          {/* TODO: navigate to `components/Profile/Profile.tsx and update all TODO:s */}
+          <Profile />
 
-        <div className="content-separator"></div>
+          <div className="content-separator"></div>
 
-        {/* TODO: navigate to `components/About/About.tsx and update all TODO:s */}
-        <About />
+          {/* TODO: navigate to `components/About/About.tsx and update all TODO:s */}
+          <About />
 
-        <div className="content-separator"></div>
+          <div className="content-separator"></div>
 
-        {/* TODO: navigate to `components/Projects/Projects.tsx and update all TODO:s */}
-        <Projects />
+          {/* TODO: navigate to `components/Projects/Projects.tsx and update all TODO:s */}
+          <Projects />
 
-        <div className="content-separator"></div>
+          <div className="content-separator"></div>
 
-        {/* TODO: navigate to `components/Activities/Activities.tsx and update all TODO:s */}
-        <Activities />
+          {/* TODO: navigate to `components/Activities/Activities.tsx and update all TODO:s */}
+          <Activities />
 
-        <div className="content-separator"></div>
+          <div className="content-separator"></div>
 
-        {/* connect Section */}
-        {/* TODO: navigate to `components/Connect/Connect.tsx and update all TODO:s */}
-        <Connect />
+          {/* connect Section */}
+          {/* TODO: navigate to `components/Connect/Connect.tsx and update all TODO:s */}
+          <Connect />
 
-        <div className="content-separator"></div>
+          <div className="content-separator"></div>
 
-        {/* TODO: navigate to `components/Chatbot/Chatbot.tsx and update all TODO:s */}
-        {/* NOTE: Chatbot is inactive (not using OpenAPI token to save money, find a free alternaitve in the future) */}
-        {/* <Chatbot /> */}
+          {/* TODO: navigate to `components/Chatbot/Chatbot.tsx and update all TODO:s */}
+          {/* NOTE: Chatbot is inactive (not using OpenAPI token to save money, find a free alternaitve in the future) */}
+          {/* <Chatbot /> */}
+        </div>
       </div>
-    </div>
+    </ThemeProvider>
   );
 }
 
